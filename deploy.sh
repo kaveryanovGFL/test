bbed1c9e8c1d6227e6eed4b41bfd6675adc7a1be
Averianov Konstantin <kaverianov@geeksforless.net>
2020-05-09 15:33:13 +0300

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ "$result" = "2Already up-to-date." ]; then
   echo "Strings are equal."
 else
   echo 'reboot'
-  kill $(ps aux | grep 'bot.py' | grep -v "grep" | cut -d " " -f2)
+  kill -l $(ps aux | grep 'bot.py' | grep -v "grep" | cut -d " " -f2)
   echo 'start'
   python3  /home/malkir/scripts/telegramBot/bot.py
 fi
