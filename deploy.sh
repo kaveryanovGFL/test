8bd7ae018035ad2f4146387f24c790b01500eaec
Averianov Konstantin <kaverianov@geeksforless.net>
2020-05-09 15:17:00 +0300

    add new echo

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 cd /home/malkir/scripts/telegramBot
 result=$(git pull)
 if [ "$result" = "2Already up-to-date." ]; then
-    echo "Strings are equal."
+  echo "Strings are equal."
 else
   echo 'reboot'
-    processId=$(ps -ef | grep 'bot.py' | grep -v 'grep' | awk '{ printf $2 }')
-    kill "$processId"
-    python3  /home/malkir/scripts/telegramBot/bot.py
+  processId=$(ps -ef | grep 'bot.py' | grep -v 'grep' | awk '{ printf $2 }')
+  kill "$processId"
+  echo 'start'
+  python3  /home/malkir/scripts/telegramBot/bot.py
 fi
