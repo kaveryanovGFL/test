2fced9514acfb8a10ac9d949002a10eab741a01f
Averianov Konstantin <kaverianov@geeksforless.net>
2020-05-09 15:13:58 +0300

    fixed bug

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ result=$(git pull)
 if [ "$result" = "2Already up-to-date." ]; then
     echo "Strings are equal."
 else
-    program=$(bot.py &)
-    kill "$program"
+    processId=$(ps -ef | grep 'bot.py' | grep -v 'grep' | awk '{ printf $2 }')
+    kill "$processId"
     python3  /home/malkir/scripts/telegramBot/bot.py
 fi
