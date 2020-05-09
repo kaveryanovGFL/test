0e80831e844734cc5fb174276991a645ad13bb50
Averianov Konstantin <kaverianov@geeksforless.net>
2020-05-09 15:09:55 +0300

    new variant to satop/start programm

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ result=$(git pull)
 if [ "$result" = "2Already up-to-date." ]; then
     echo "Strings are equal."
 else
-    pkill -f  /home/malkir/scripts/telegramBot/bot.py
+    program=$(bot.py &)
+    kill "$program"
     python3  /home/malkir/scripts/telegramBot/bot.py
 fi
